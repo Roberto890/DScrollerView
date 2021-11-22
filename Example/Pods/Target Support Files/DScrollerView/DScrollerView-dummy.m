f231d235cb018e4f66b33ed66fcd643755037406
Roberto890 <roberto.jesus.amaral@gmail.com>
2021-11-22 09:32:18 -0300

    ínitial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/DScrollerView/DScrollerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DScrollerView : NSObject
+@end
+@implementation PodsDummy_DScrollerView
+@end
---
+++ Example/Pods/Target Support Files/Pods-DScrollerView_Tests/Pods-DScrollerView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DScrollerView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DScrollerView_Tests
+@end
